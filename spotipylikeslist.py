a933f6b68e72ae4d4bc52cb050ea55ea464d27d9
Dale Collison <dale.a.collison@gmail.com>
2023-09-22 10:32:52 +0100

    Updates

--- spotipylikeslist.py
+++ spotipylikeslist.py
@@ -8,13 +8,18 @@ from auth import *
 from config import *
 
 if __name__ == "__main__":
-
     # Set up API to read User Information
     scope = "user-library-read"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret,
-                                                   redirect_uri=redirect_uri, scope=scope))
+    sp = spotipy.Spotify(
+        auth_manager=SpotifyOAuth(
+            client_id=client_id,
+            client_secret=client_secret,
+            redirect_uri=redirect_uri,
+            scope=scope,
+        )
+    )
 
-    username = sp.me()['id']
+    username = sp.me()["id"]
 
     # Gather tracks into playlists
     playlists: dict[str, list[str]] = {}
@@ -23,9 +28,9 @@ if __name__ == "__main__":
 
     results = sp.current_user_saved_tracks(result_limit, offset)
 
-    while results['items']:
-        for item in results['items']:
-            dt_object = datetime.datetime.strptime(item['added_at'], dt_format)
+    while results["items"]:
+        for item in results["items"]:
+            dt_object = datetime.datetime.strptime(item["added_at"], dt_format)
 
             if dt_object.year < oldest_year:
                 continue
@@ -35,12 +40,14 @@ if __name__ == "__main__":
             if playlist_name not in playlists:
                 playlists[playlist_name] = []
 
-            playlists[playlist_name].append(item['track']['id'])
+            playlists[playlist_name].append(item["track"]["id"])
 
-            print(f"Added to playlist "
-                  f"'{dt_object.year}.{ceil(dt_object.month / increment)}': "
-                  f"{item['track']['name']} - "
-                  f"{', '.join([x['name'] for x in item['track']['artists']])}")
+            print(
+                f"Added to playlist "
+                f"'{dt_object.year}.{ceil(dt_object.month / increment)}': "
+                f"{item['track']['name']} - "
+                f"{', '.join([x['name'] for x in item['track']['artists']])}"
+            )
 
         offset += result_limit
         results = sp.current_user_saved_tracks(result_limit, offset)
@@ -48,9 +55,13 @@ if __name__ == "__main__":
     # Set up API to create playlists
     scope = "playlist-modify-private"
     sp = spotipy.Spotify(
-        auth_manager=SpotifyOAuth(client_id=client_id,
-                                  client_secret=client_secret,
-                                  redirect_uri=redirect_uri, scope=scope))
+        auth_manager=SpotifyOAuth(
+            client_id=client_id,
+            client_secret=client_secret,
+            redirect_uri=redirect_uri,
+            scope=scope,
+        )
+    )
 
     for playlist_name, playlist_tracks in reversed(playlists.items()):
         pl = sp.user_playlist_create(
@@ -58,10 +69,10 @@ if __name__ == "__main__":
             name=playlist_name + " [SpotipyLikesList]",
             public=False,
             collaborative=False,
-            description='Created using SpotipyLikesList.')
+            description="Created using SpotipyLikesList.",
+        )
 
-        sp.playlist_add_items(playlist_id=pl['id'],
-                              items=playlist_tracks)
+        sp.playlist_add_items(playlist_id=pl["id"], items=playlist_tracks)
 
     total_tracks = sum(len(x) for x in playlists.values())
     print(f"Added {total_tracks} tracks to {len(playlists)} playlists.")
